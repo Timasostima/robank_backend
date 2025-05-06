96c2a889443a0ef33f04dac8fedd19ad7f029bd7
Tymur Kulivar <contact@tymurkulivar.dev>
2025-05-06 09:36:08 +0200

    restructure

--- src/main/java/dev/tymurkulivar/robankApi/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.tymurkulivar.robankApi.repositories;
-
-import dev.tymurkulivar.robankApi.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
