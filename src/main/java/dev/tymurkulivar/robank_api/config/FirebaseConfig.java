d1c03686995d27338dfd7cfffce728ed11622cf1
Tymur Kulivar <contact@tymurkulivar.dev>
2025-05-16 17:51:48 +0200

    Update the firebase configuration

--- src/main/java/dev/tymurkulivar/robank_api/config/FirebaseConfig.java
+++ src/main/java/dev/tymurkulivar/robank_api/config/FirebaseConfig.java
@@ -6,22 +6,37 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.util.Map;
 
 @Configuration
 public class FirebaseConfig {
     @PostConstruct
     public void initializeFirebase() {
         try {
-            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
+            Map<String, String> env = System.getenv();
 
-            if (serviceAccount == null) {
-                throw new IllegalStateException("Firebase serviceAccountKey.json not found in resources folder.");
-            }
+            String projectId = getEnvVariable(env, "FIREBASE_PROJECT_ID");
+            String privateKeyId = getEnvVariable(env, "FIREBASE_PRIVATE_KEY_ID");
+            String privateKey = getEnvVariable(env, "FIREBASE_PRIVATE_KEY").replace("\\n", "\n");
+            String clientEmail = getEnvVariable(env, "FIREBASE_CLIENT_EMAIL");
+            String clientId = getEnvVariable(env, "FIREBASE_CLIENT_ID");
+            String firebaseX509 = getEnvVariable(env, "FIREBASE_x509");
 
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+            final String firebaseConfig = "{"
+                + "\"type\": \"service_account\","
+                + "\"project_id\": \"" + projectId + "\","
+                + "\"private_key_id\": \"" + privateKeyId + "\","
+                + "\"private_key\": \"" + privateKey + "\","
+                + "\"client_email\": \"" + clientEmail + "\","
+                + "\"client_id\": \"" + clientId + "\","
+                + "\"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\","
+                + "\"token_uri\": \"https://oauth2.googleapis.com/token\","
+                + "\"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\","
+                + "\"client_x509_cert_url\": \"" + firebaseX509 + "\""
+                + "}";
+
+            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfig.getBytes()))).build();
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
@@ -29,5 +44,13 @@ public class FirebaseConfig {
         } catch (Exception e) {
             throw new RuntimeException("Failed to initialize Firebase", e);
         }
+    }
+
+    private String getEnvVariable(Map<String, String> env, String key) {
+        String value = env.get(key);
+        if (value == null) {
+            throw new IllegalArgumentException("Environment variable " + key + " is not set.");
+        }
+        return value;
     }
 }
